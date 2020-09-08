27476967d4534988459758672e823eb422544ad5
nandinired <70929389+nandinired@users.noreply.github.com>
2020-09-07 20:37:29 -0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ import requests
 def lambda_handler(event, context):
     # TODO implement
     # api-endpoint
-    print(event1)
+    print(event)
     URL = "http://maps.googleapis.com/maps/api/geocode/json"
     location = event['landmark']
     # defining a params dict for the parameters to be sent to the API
