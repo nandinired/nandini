db4783dbe891b1319fbb4438fdaacdf0f9129511
nandinired <70929389+nandinired@users.noreply.github.com>
2020-09-15 20:20:58 -0700

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,8 @@ import os
 import shutil
 import boto3
 def lambda_handler(event, context):
-    # TODO implement
-    # api-endpoint
   source = 'develop/'
-  dest1 = '/mnt/access'
+  dest1 = '/mnt/src'
   files = os.listdir(source)
   for f in files:
     shutil.copy(source+f, dest1)
