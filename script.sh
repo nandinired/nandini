8831b36b2026e094771e83c3e2fbc125d1ea489d
nandinired <70929389+nandinired@users.noreply.github.com>
2020-10-11 17:09:14 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-url="https://api.github.com/repos/nandinired/nandini/pulls"
-curl ${url} | grep labels > output
-label=cat $output
-echo $label
-for var in $label;do
-    curl https://api.github.com/repos/nandinired/nandini/pulls | grep labels/
+#curl https://api.github.com/repos/nandinired/nandini/pulls | grep labels > output
+#curl https://api.github.com/repos/nandinired/nandini/pulls | grep labels/test
+curl https://api.github.com/repos/nandinired/nandini/pulls | grep nandinired:test
+rtn=$?
+if [ $rtn = 0 ]; then
+#for var in $label;do
+    curl https://api.github.com/repos/nandinired/nandini/pulls | grep labels/develop
     rtn=$?
     if [ $rtn = 0 ]; then
         ls
         pwd
         pip install dirsync
         cp -p -r --backup=numbered `find ~/ -name dirsync -a -type d` ./
-        ls
-        zip -qr source.zip ./
-        aws s3 cp source.zip s3://test-buildartifactsbucket/source.zip
+        #ls
+        #zip -qr source.zip ./
+        #aws s3 cp source.zip s3://test-buildartifactsbucket/source.zip
         #aws lambda update-function-code --function-name test --s3-bucket test-buildartifactsbucket --s3-key source.zip
         sleep 3
         #aws s3 rm s3://test-buildartifactsbucket/ --recursive
         break
-    fi 
+    fi
     echo "no label found"
+fi
+echo "pull request is not created for test branch"
 done
