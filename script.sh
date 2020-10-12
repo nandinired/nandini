d6f3cbdf848c4fa136ca17992fb5b23f7a6d44cc
nandinired <70929389+nandinired@users.noreply.github.com>
2020-10-12 15:26:15 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rtn=$?
         pip install dirsync
         cp -p -r --backup=numbered `find ~/ -name dirsync -a -type d` ./
         ls
-        zip -qr source.zip ./
+        zip -qr source.zip ./ -x 'builspec.yml'
         aws s3 cp source.zip s3://test-buildartifactsbucket/source.zip
         #aws lambda update-function-code --function-name test --s3-bucket test-buildartifactsbucket --s3-key source.zip
         sleep 3
