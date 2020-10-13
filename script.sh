2247701ba4ece00d0bec014f78dc0912c58e7d42
nandinired <70929389+nandinired@users.noreply.github.com>
2020-10-12 18:20:39 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,10 +6,12 @@ rtn=$?
         pwd
         pip install dirsync
         cp -p -r --backup=numbered `find ~/ -name dirsync -a -type d` ./
-        ls
+        cd .. && mkdir temp && cd temp && mv nandini/buildspec.yml ./
+        cd nandini
         zip -qr source.zip ./ --exclude=buildspec.yml/
         aws s3 cp source.zip s3://test-buildartifactsbucket/source.zip
         #aws lambda update-function-code --function-name test --s3-bucket test-buildartifactsbucket --s3-key source.zip
+        mv ../temp/buildspec.yml nandini
         sleep 3
         #aws s3 rm s3://test-buildartifactsbucket/ --recursive
     else
